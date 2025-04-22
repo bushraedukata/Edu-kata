4ca823038ee166c0371c234f3549420cf0a15326
bushraedukata <bushra0ahmed@gmail.com>
2025-04-22 19:21:11 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentQuiz from './StudentQuiz';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Edu Kata</h1>
+      <StudentQuiz />
+    </div>
+  );
+}
+
+export default App;
